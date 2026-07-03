1d474458b69f6f50626ad5b88dfa6abd4de6078f
kalea <kalea@192.168.10.36>
2026-07-03 16:55:15 +0530

    Commit csv read file

---
+++ EclipseProject/src/GeneExample/FirstCSVExample.java
@@ -0,0 +1,44 @@
+package GeneExample;
+
+import java.io.FileReader;
+import java.io.Reader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class FirstCSVExample {
+
+	public void readfood() {
+	    try {
+	        Reader reader = new FileReader("src/GeneExample/food.csv");
+
+	        CSVParser parser = CSVFormat.DEFAULT.builder()
+	                .setHeader()
+	                .setSkipHeaderRecord(true)
+	                .get()
+	                .parse(reader);
+
+	        for (CSVRecord record : parser) {
+	            String name = record.get("Name");
+	            String favoriteColor = record.get("Favorite Color");
+	            String favoriteFood = record.get("Favorite Food");
+
+	            System.out.println("Name: " + name);
+	            System.out.println("Favorite Color: " + favoriteColor);
+	            System.out.println("Favorite Food: " + favoriteFood);
+	            System.out.println("----------------------------");
+	        }
+
+	        parser.close();
+	        reader.close();
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+    public static void main(String[] args) {
+        FirstCSVExample obj = new FirstCSVExample();
+        obj.readfood();
+    }
+}
